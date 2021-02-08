90ee758ddbce3720aecebcbd39ef4745d638e68d
AndrejKolesnikas <70938154+AndrejKolesnikas@users.noreply.github.com>
2021-02-08 11:43:25 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,78 @@
+//alert('E');
+
+//prompt('A');
+
+//document.write('S');
+
+//document.GetElementById('....').innerHTML ="Insert text";
+
+// console.log('Y E E T');
+// let a = 5;
+// console.log(a);
+
+// console.error('You don goofed');
+// console.warn('You almost goofed');
+
+// var name ='John Cena';
+// var pass;
+// pass = 'Doot';
+
+//let name = "John";
+//let name = 'Petah'
+
+//(log must be below elements)
+//console.log(number1);
+//console.log(number2);
+
+// var number1 = 20;
+// let number2 = 40;
+//
+// console.log(number1);
+// console.log(number2);
+
+
+// const number1 = 25;
+// console.log(number1);
+//
+// number1 = 24;
+
+// const name = 'JoJo';
+// const number1 = 24;
+// const number2 = 5.7;
+// const isTall = true;
+// const z = null;
+// const t = undefined;
+// let q;
+
+// let name = 'pen';
+// let surname = 'pineapple'
+// console.log('Pen' + ' pineapple ' + 'applepen ');
+//
+// console.log(` my name is ${pen}, and last name ${pineapple}`);
+
+// let name = 'joseph';
+//
+// console.log(name.indexOf('t'));
+
+// let name = 'joseph';
+// let eman = name.split(' ')
+//console.log(name.slice('1'));
+
+// let items = new Array();
+// items[0] = 'item1';
+// items[1] = 'item2';
+// items[2] = 'item3';
+// items[3] = 'item4';
+// items[4] = 'item5';
+// console.log(items);
+//
+// let items2 = ['thing1', 'thing2', 'thing3', 'thing4'];
+// console.log(items2);
+
+
+ // let phone = new Object();
+ // phone ['developer'] = 'nintendo';
+ // phone ['color'] = 'blue dabudi dabudai';
+ // phone ['model'] = 'new';
+ //
+ // console.log(phone)
